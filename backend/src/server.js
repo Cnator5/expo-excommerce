36ca634fd6585268e2be5e5933f17ca9a5157cc1
Senator Lambi <beyoh172@gmail.com>
2025-12-30 20:43:37 +0100

    deleted node modules

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,30 @@
 import express from "express";
+import path from "path"
+import { ENV } from "./config/env.js";
 
 const app = express();
 
 // Middleware
 app.use(express.json());
 
+const __dirname = path.resolve()
+
+
 // Health check route
 app.get("/api/health", (req, res) => {
   res.status(200).json({ message: "Success" });
 });
 
+//make the mobile app ready for deployment
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../admin/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../admin", "dist", "index.html"));
+  });
+}
+
 // Start server
-const PORT = 1010;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+app.listen(ENV.PORT, () => {
+  console.log("Server is running");
 });
