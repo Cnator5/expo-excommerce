2293bd9ed89e7ca7241c045c9f2a4ed66c65fa45
Senator Lambi <beyoh172@gmail.com>
2025-12-31 10:46:48 +0100

    auth implemented

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,41 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  const increment = () =>setCount(count +1);
-  const decrement = () =>setCount(c=>Math.max(0, c-1))
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <h1>{count}</h1>
-        <button onClick={decrement}>
-         -
-        </button>
-        <button onClick={increment}>
-         +
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+        <SignedOut>
+        <SignInButton mode='modal' />
+      </SignedOut>
+
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
+    </div>
   )
 }
 
