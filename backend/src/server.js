f9ada0f296300ed381e9c4262efb116734bb2451
Senator Lambi <beyoh172@gmail.com>
2026-01-01 14:15:12 +0100

    admin dashboard

--- backend/src/server.js
+++ backend/src/server.js
@@ -65,7 +65,7 @@ if (ENV.NODE_ENV === "production") {
 const startServer = async () => {
   await connectDB();
   app.listen(ENV.PORT, () => {
-    console.log("Server is up and running");
+    console.log("Server is up and running on port:", ENV.PORT);
   });
 };
 
