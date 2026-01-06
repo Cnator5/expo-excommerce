ce306ac1e731725f99ba7f598bde58d47806eb62
Senator Lambi <beyoh172@gmail.com>
2026-01-06 04:23:13 +0100

    wishlist screen added

---
+++ mobile/app/(profile)/addresses.tsx
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+
+const AdressesScreen = () => {
+  return (
+    <View>
+      <Text>AdressesScreen</Text>
+    </View>
+  )
+}
+
+export default AdressesScreen
